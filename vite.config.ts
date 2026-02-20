159ce037522d8be49b75ef496fa2852ecba3f4bc
lm16688 <121564098+lm16688@users.noreply.github.com>
2026-02-20 10:23:07 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: process.env.GITHUB_ACTIONS ? '/novaclip-ai/' : '/', // 添加这一行
       server: {
         port: 3000,
         host: '0.0.0.0',
